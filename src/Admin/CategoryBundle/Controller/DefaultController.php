dba8f1ff0bdcccb203b0beefa6d658d5241e1f0d
ebara10 <ebara10@c.kyoai.ac.jp>
2015-03-19 16:21:29 +0900

    update タブ

--- src/Admin/CategoryBundle/Controller/DefaultController.php
+++ src/Admin/CategoryBundle/Controller/DefaultController.php
@@ -13,39 +13,37 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-		//Productテーブルの中身を$repositoryに入れる
+	//Productテーブルの中身を$repositoryに入れる
         $repository = $this->getDoctrine()
-		->getRepository('AdminCategoryBundle:Product');
-		//全ての商品をfind(SELECT * FROM Product)
-		$product = $repository->findAll();
+	->getRepository('AdminCategoryBundle:Product');
+	//全ての商品をfind(SELECT * FROM Product)
+	$product = $repository->findAll();
 
         return $this->render('AdminCategoryBundle:Default:index.html.twig', array('product' => $product));
     }
 
     public function newAction(Request $request)
     {
-		//productオブジェクトの生成
+	//productオブジェクトの生成
         $product = new Product();
 
-		//formの作成
+	//formの作成
         $form = $this->createFormBuilder($product)
-			->add('name', 'text')
-			->getForm();
-		if ($request->getMethod() == 'POST') {
-			//bindRequestが呼び出された時点でフォームに反映される
- 			$form->bind($request);
-			if ($form->isValid()) {
-
-				//Entityマネージャー
-    		    $em = $this->getDoctrine()->getEntityManager();
-				//プログラム側のエンティティに追加
-    		    $em->persist($product);
-				//データベースに反映
-    		    $em->flush();
-
-				return $this->redirect($this->generateUrl('product'));
-			}
+		->add('name', 'text')
+		->getForm();
+	if ($request->getMethod() == 'POST') {
+		//bindRequestが呼び出された時点でフォームに反映される
+ 		$form->bind($request);
+		if ($form->isValid()) {
+			//Entityマネージャー
+			$em = $this->getDoctrine()->getEntityManager();
+			//プログラム側のエンティティに追加
+			$em->persist($product);
+			//データベースに反映
+			$em->flush();
+			return $this->redirect($this->generateUrl('product'));
 		}
+	}
         return $this->render('AdminCategoryBundle:Default:new.html.twig', array('product' => $product,  'form' => $form->createView()));
 
 
@@ -61,11 +59,11 @@ class DefaultController extends Controller
 			throw $this->createNotFoundException('No product found for id '.$id);
 		}
 
-		$editForm = $this->createEditForm($product);
+	$editForm = $this->createEditForm($product);
 
         return $this->render('AdminCategoryBundle:Default:edit.html.twig', array(
-            'product'      => $product,
-            'edit_form'   => $editForm->createView(),
+        	'product'      => $product,
+        	'edit_form'   => $editForm->createView(),
         ));
 	}
 	private function createEditForm($product)
@@ -74,39 +72,34 @@ class DefaultController extends Controller
             'action' => $this->generateUrl('product_update', array('id' => $product->getId())),
             'method' => 'PUT',
         ));
-
         $form->add('submit', 'submit', array('label' => 'Update'));
 
         return $form;
 	}
-	//
+
 	public function updateAction(Request $request, $id)
 	{
         $em = $this->getDoctrine()->getManager();
-
         $product = $em->getRepository('AdminCategoryBundle:Product')->find($id);
 
         if (!$product) {
             throw $this->createNotFoundException('Unable to find post entity.');
         }
-		//formの作成
+	//formの作成
         $editForm = $this->createEditForm($product);
-		//requestとformをバインドさせる
+	//requestとformをバインドさせる
         $editForm->handleRequest($request);
 
-		//値のチェック
+	//値のチェック
         if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('product', array('id' => $id)));
+        	$em->flush();
+		return $this->redirect($this->generateUrl('product', array('id' => $id)));
         }
 
         return $this->render('AdminCategoryBundle:post:edit.html.twig', array(
-            'product'      => $product,
-            'edit_form'   => $editForm->createView(),
+        	'product'      => $product,
+        	'edit_form'   => $editForm->createView(),
         ));
-
-
 	}
 
 	public function deleteAction(Request $request, $id)
@@ -118,7 +111,7 @@ class DefaultController extends Controller
 		}
 		$em->remove($product);
 		$em->flush();
-	return $this->render('AdminCategoryBundle:Default:delete.html.twig', array('product' => $product, 'id'=> $id));
+		return $this->render('AdminCategoryBundle:Default:delete.html.twig', array('product' => $product, 'id'=> $id));
 	}
 
 
