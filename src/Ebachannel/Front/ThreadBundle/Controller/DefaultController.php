18871f09907c6edf4fe87c8abed8ef83371cbc52
“chihiro.ebara” <chihiro.ebara@net-marketing.co.jp>
2015-03-30 22:37:01 +0900

    entity

--- src/Ebachannel/Front/ThreadBundle/Controller/DefaultController.php
+++ src/Ebachannel/Front/ThreadBundle/Controller/DefaultController.php
@@ -2,17 +2,24 @@
 
 namespace Ebachannel\Front\ThreadBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Ebachannel\Admin\CategoryBundle\Entity\category;
+use Ebachannel\Front\ThreadBundle\Entity\thread;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction($id)
     {
-        //categoryテーブルの中身を$repositoryに入れる
-        $repository = $this->getDoctrine()->getRepository('EbachannelFrontThreadBundle:category');
-        //全ての商品をfind(SELECT * FROM thread)
-        $category = $repository->findAll();
-        return $this->render('EbachannelFrontThreadBundle:Default:index.html.twig', array('category' => $category));
+        $em = $this->getDoctrine()->getEntityManager();
+        $category = $em->getRepository('EbachannelAdminCategoryBundle:category')->find($id);
+        $thread = $em->getRepository('EbachannelFrontThreadBundle:thread')->findAll($id);
+        
+        //該当のidが見つからなかった時の処理
+        if (!$category) {
+            throw $this->createNotFoundException('No category found for id '.$id);
+        }
+        return $this->render('EbachannelFrontThreadBundle:Default:index.html.twig', array('category' => $category, 'id' =>$id));
 
     }
 }
